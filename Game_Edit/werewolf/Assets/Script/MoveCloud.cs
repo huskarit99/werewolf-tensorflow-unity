d91e22e082073031ed3c71d8f39f4e9cb8d109a1
Hoang Vo Minh <hoangvm.hiephoa@gmail.com>
2021-03-15 17:08:03 +0700

    add: Cloud movement

---
+++ Game_Edit/werewolf/Assets/Script/MoveCloud.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class MoveCloud : MonoBehaviour
+{
+    public float MoveSpeed_Max = 20;
+    public float MoveSpead_Min = 15;
+
+    GameObject EndPoint_Cloud;
+    private GameObject Cloud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndPoint_Cloud = GameObject.FindGameObjectWithTag("EndPoint_Cloud");
+        Cloud = GameObject.FindGameObjectWithTag("Cloud");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if (EndPoint_Cloud == null)
+        {
+            return;
+        }
+        var endPoint_Cloud = new GameObject();
+        endPoint_Cloud.transform.position = new Vector3(EndPoint_Cloud.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        transform.LookAt(endPoint_Cloud.transform.position);
+        var moveSpeed = Random.Range(MoveSpead_Min, MoveSpeed_Max);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (EndPoint_Cloud.transform.position.x <= transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game_Edit/werewolf/Assets/Script/SpawnCloud.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCloud : MonoBehaviour
+{
+    public GameObject[] Clouds;
+    public GameObject StartPoint_Cloud;
+    public GameObject EndPoint_Cloud;
+    //---Spawn Time---//
+    private float SpawnTime_Min = 6;
+    private float SpawnTime_Max = 8;
+    private float LastSpawnTime = 0;
+    private float SpawnTime = 0;
+    //---Spawn position---//
+    private float DiffPosition_Y_Min = -40;
+    private float DiffPosition_Y_Max = 30;
+    private float DiffPosition_Z_Min = -10;
+    private float DiffPosition_Z_Max =  5;
+    private float LastDiffPosition_Y = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateSpawnTime();
+    }
+    void UpdateSpawnTime()
+    {
+        LastSpawnTime = Time.time;
+        SpawnTime = Random.Range(SpawnTime_Min, SpawnTime_Max);
+    }
+    void Spawn()
+    {
+        var cloud = Clouds[Random.Range(0, Clouds.Length)];
+        LastDiffPosition_Y = Random.Range(DiffPosition_Y_Min, DiffPosition_Y_Max);
+        var position = new Vector3(StartPoint_Cloud.transform.position.x,
+                                    StartPoint_Cloud.transform.position.y + LastDiffPosition_Y,
+                                    StartPoint_Cloud.transform.position.z + Random.Range(DiffPosition_Z_Min,DiffPosition_Z_Max));
+        Instantiate(cloud, position, Quaternion.identity);
+        UpdateSpawnTime();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= LastSpawnTime + SpawnTime)
+        {
+            Spawn();
+        }
+        
+    }
+}
